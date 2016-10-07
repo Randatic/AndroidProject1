eb31d765b480d20ac234510cd07570b5c75171a3
Ellis <espellerberg@gmail.com>
2016-10-07 13:47:14 -0700

    Character Class

---
+++ app/src/main/java/io/github/randatic/tesgame/Character.java
@@ -0,0 +1,39 @@
+package io.github.randatic.tesgame;
+
+/**
+ * Created by Ellis on 10/7/2016.
+ */
+public class Character {
+    private int hp, atk, def, spd, xp, maxXp, lvl;
+    public Character(){
+        hp = 100;
+        atk = 10;
+        def = 10;
+        spd = 10;
+        xp = 0;
+        maxXp = 50;
+        lvl = 1;
+
+        if(xp == maxXp){
+            levelUp();
+        }
+    }
+
+    private void levelUp() {
+        lvl++;
+    }
+
+    public void setAtk(int atk){
+        this.atk += atk;
+    }
+
+    public void setDef(int def){
+        this.def += def;
+    }
+
+    public void setSpd(int spd){
+        this.spd += spd;
+    }
+
+}
+
