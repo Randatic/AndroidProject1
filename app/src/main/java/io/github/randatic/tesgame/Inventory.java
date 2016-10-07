b8f07593531dc4ae2a36a24bb67cbc17fa8b35ce
csaper6 <csaper6@CompSci-33.local>
2016-10-07 13:14:34 -0700

    Initial commit

---
+++ app/src/main/java/io/github/randatic/tesgame/Inventory.java
@@ -0,0 +1,54 @@
+package io.github.randatic.tesgame;
+
+import java.util.ArrayList;
+/**
+ * Created by Randy on 10/3/16.
+ */
+public class Inventory {
+
+    ArrayList<Item> inventory;
+
+    public Inventory () {
+        inventory = new ArrayList<Item>();
+    }
+
+    public void addItem(Item i) {
+        inventory.add(i);
+    }
+
+    public Item getItem(int index) {
+        return inventory.get(index);
+    }
+
+    public void useItem() {
+
+    }
+
+    public int hasItem(Item i) {
+        for (Item e: inventory) {
+            if()
+        }
+    }
+
+    public Item tradeItem(int oldItem, Item newItem) {
+        inventory.add(oldItem, newItem);
+        return inventory.remove(oldItem+1);
+    }
+
+    public void removeItem(int index) {
+        inventory.remove(index);
+    }
+
+    public void removeItem(Item i) {
+        inventory.remove(i);
+    }
+
+    public void updateInventory() {
+        int[] toRemove = new int[inventory.size()];
+        for(int i = inventory.size()-1; i<=0; i--) {
+            if (inventory.get(i).getValue()==0) {
+                inventory.remove(i);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/io/github/randatic/tesgame/MapTestActivity.java
@@ -0,0 +1,33 @@
+package io.github.randatic.tesgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MapTestActivity extends AppCompatActivity {
+    private String[][] localMap = {
+        {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
+        {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
+        {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
+        {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
+        {"#", "#", "#", "#", "@", "#", "#", "#", "#"},
+        {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
+        {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
+        {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
+        {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
+    };
+
+    private TextView map;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_test);
+
+        map = (TextView) findViewById(R.id.textView);
+
+    }
+
+    public void updateMap() {
+
+    }
+}
