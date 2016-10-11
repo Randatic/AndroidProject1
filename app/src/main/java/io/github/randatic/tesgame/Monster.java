5b2c1136e901b0e2422d54114f81338a8ace4e6c
Ellis <espellerberg@gmail.com>
2016-10-11 13:43:32 -0700

    Fix:
    Add:
    MonsterSpawner Java
    
    Change:
    Monster Java

--- app/src/main/java/io/github/randatic/tesgame/Monster.java
+++ app/src/main/java/io/github/randatic/tesgame/Monster.java
@@ -4,21 +4,48 @@ package io.github.randatic.tesgame;
  * Created by Everyone on 10/7/16.
  */
 public class Monster {
-    private int atk, def, spd, hp;
-    private String name;
+    private int atk, def, spd, hp, xp;
+    private String name, area;
+    private char icon;
+    private boolean canFlee;
 
-    public Monster(int area){
-        if (area == 5){
-            spawnSlime();
-        }
+    public Monster(){
+
     }
 
-    private void spawnSlime() {
+    private void Slime() {
         name = "Slime";
+        icon = 'S';
+        area = "Plains";
         hp = 10;
         atk = 5;
         def = 5;
         spd = 5;
+        xp = 10;
+        canFlee = false;
+    }
+
+    private void Wolf() {
+        name = "Wolf";
+        icon = 'W';
+        area = "Plains";
+        hp = 15;
+        atk = 10;
+        def = 5;
+        spd = 15;
+        xp = 15;
+        canFlee = false;
+    }
+
+    private void Deer() {
+        name = "Deer";
+        icon = 'D';
+        hp = 20;
+        atk = 0;
+        def = 20;
+        spd = 20;
+        xp = 20;
+        canFlee = true;
     }
 
 }
