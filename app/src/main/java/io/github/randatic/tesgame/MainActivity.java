85dced99798400020829ddf65cf03ed71a98406c
csaper6 <kmyohan0@gmail.com>
2016-10-17 12:32:29 -0700

    characters and character activity is edited  <!--Yohan Kim-->

--- app/src/main/java/io/github/randatic/tesgame/MainActivity.java
+++ app/src/main/java/io/github/randatic/tesgame/MainActivity.java
@@ -3,6 +3,8 @@ package io.github.randatic.tesgame;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,21 +13,69 @@ public class MainActivity extends AppCompatActivity {
     private Map map;
     private Place[] places;
     private Character character;
+    private TextView eventShow;
+    private Button up, down, left, right, action;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wirewidget();
+        displayMap();
+        displayEvent();
+        onclick();
+    }
 
+    private void onclick()
+    {
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void wirewidget()
+    {
         places = new Place[] {};
         character = new Character();
-
         tvMap = (TextView) findViewById(R.id.textViewMap);
+        up = (Button) findViewById(R.id.buttonUp);
+        down = (Button) findViewById(R.id.buttonDown);
+        left = (Button) findViewById(R.id.buttonLeft);
+        right = (Button) findViewById(R.id.buttonRight);
+        action = (Button) findViewById(R.id.buttonAction);
         tvMap.setTypeface(Typeface.MONOSPACE); //makes characters the same size
-
+        eventShow = (TextView) findViewById(R.id.textViewEvents);
         map = new Map(places, character);
-        displayMap();
     }
+
+
 
     public void displayMap() {
         String display = "";
@@ -38,4 +88,11 @@ public class MainActivity extends AppCompatActivity {
 
         tvMap.setText(display);
     }
+
+
+    public void displayEvent()
+    {
+
+    }
+
 }
