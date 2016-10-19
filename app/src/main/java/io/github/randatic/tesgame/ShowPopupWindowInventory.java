c8a5a23628931ee5fdc3785dacdbf4e3c3d333dd
csaper6 <kmyohan0@gmail.com>
2016-10-19 13:15:46 -0700

    characters and character activity is edited  <!--Yohan Kim-->

--- app/src/main/java/io/github/randatic/tesgame/ShowPopupWindowInventory.java
+++ app/src/main/java/io/github/randatic/tesgame/ShowPopupWindowInventory.java
@@ -2,15 +2,25 @@ package io.github.randatic.tesgame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 /**
  * Created by csaper6 on 10/17/16.
  */
 public class ShowPopupWindowInventory extends Activity {
-    protected void onCreate(Bundle savedinstancesState)
+    protected void onCreate(Bundle savedinstanceState)
     {
-        super.onCreate(savedinstancesState);
+        super.onCreate(savedinstanceState);
+
         setContentView(R.layout.showpopupwindowinventory);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width * .91), (int)(height * .6));
 
 
 //TODO: make columns from layout appear as same number as inventory items
