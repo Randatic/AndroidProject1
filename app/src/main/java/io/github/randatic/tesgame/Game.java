89b92ea55e6cd23e000cff137784b6d20e3cd3d7
Randy <randatic@gmail.com>
2016-10-19 13:11:25 -0700

    added game class - Randy

--- app/src/main/java/io/github/randatic/tesgame/Game.java
+++ app/src/main/java/io/github/randatic/tesgame/Game.java
@@ -4,5 +4,19 @@ package io.github.randatic.tesgame;
  * Created by csaper6 on 10/17/16.
  */
 public class Game {
+    private Map map;
+    private Character player;
+
+    public Game () {
+        player = new Character();
+        map = new Map(player);
+    }
+
+    public Character getPlayer() {
+        return player;
+    }
+    public Map getMap() {
+        return map;
+    }
 
 }
