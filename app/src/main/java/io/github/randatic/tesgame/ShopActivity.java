afab0cfadea89178a671ebfd49117b3d87468e1c
Ellis <espellerberg@gmail.com>
2016-10-21 13:42:34 -0700

    Updates

---
+++ app/src/main/java/io/github/randatic/tesgame/ShopActivity.java
@@ -0,0 +1,27 @@
+package io.github.randatic.tesgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Ellis on 10/21/2016.
+ */
+public class ShopActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button item1, item2, item3;
+    private TextView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
